2b04b861c317c5da2519468aed84b8171e3b3a54
njay <ninogaringarao13@gmail.com>
2025-01-03 21:28:53 +0800

    Fix (components) : Resolve missing dependencies including but not limited to npm package

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,58 @@
+import GlobalProvider from "@/context/GlobalProvider";
 import { Stack } from "expo-router";
-import "../global.css";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { RootSiblingParent } from "react-native-root-siblings";
+import React from "react";
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <SafeAreaView className="h-full w-full">
+        <GlobalProvider>
+          <RootSiblingParent>
+            <Stack>
+              <Stack.Screen name="index" options={{ headerShown: false }} />
+              <Stack.Screen
+                name="settings"
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="line"
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="createLinePage"
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="createPostPage"
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="(auth)"
+                options={{
+                  headerShown: false,
+                }}
+              />
+
+              <Stack.Screen
+                name="(tabs)"
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack>
+          </RootSiblingParent>
+        </GlobalProvider>
+      </SafeAreaView>
+    </>
+  );
 }
