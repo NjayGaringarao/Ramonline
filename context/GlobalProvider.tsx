4ecb26ce8a6ac6efd3280b99fcda7212d616275f
njay <ninogaringarao13@gmail.com>
2025-01-10 14:28:33 +0800

    Modified (GlobalProvider) : Implement new implementation

--- context/GlobalProvider.tsx
+++ context/GlobalProvider.tsx
@@ -1,149 +1,162 @@
-// import React, {
-//   createContext,
-//   useContext,
-//   useState,
-//   useEffect,
-//   Dispatch,
-//   ReactNode,
-// } from "react";
-// import {
-//   requestNotificationPermissions,
-//   getFCMToken,
-//   setupNotificationHandlers,
-// } from "@/services/notificationServices";
-// import { getCurrentUser, getUserInfo } from "@/services/userServices";
-// import { setupPushTarget } from "@/services/notificationServices";
-// import { ExtendedUserType } from "@/constants/types";
-// import { Models } from "react-native-appwrite";
-// import { useNetworkState } from "expo-network";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  Dispatch,
+  ReactNode,
+} from "react";
+import {
+  requestNotificationPermissions,
+  getFCMToken,
+  setupNotificationHandlers,
+} from "@/services/notificationServices";
+import {
+  getCurrentUser,
+  getUserActivity,
+  getUserInfo,
+} from "@/services/userServices";
+import { setupPushTarget } from "@/services/notificationServices";
+import { UserType } from "@/types/models";
+import { Models } from "react-native-appwrite";
+import { useNetworkState } from "expo-network";
 
-// type GlobalContextProps = {
-//   children: ReactNode;
-// };
+type GlobalContextProps = {
+  children: ReactNode;
+};
 
-// export interface GlobalContextInterface {
-//   user: Models.User<Models.Preferences> | null;
-//   setUser: Dispatch<
-//     React.SetStateAction<Models.User<Models.Preferences> | null>
-//   >;
-//   isLoading: boolean;
-//   setUserInfo: Dispatch<React.SetStateAction<ExtendedUserType>>;
-//   userInfo: ExtendedUserType;
-//   fcmToken: string;
-//   setFcmToken: Dispatch<React.SetStateAction<string>>;
-//   setIsRefreshUserInfo: Dispatch<React.SetStateAction<boolean>>;
-//   isRefreshUserInfo: boolean;
-//   setIsRefreshFeeds: Dispatch<React.SetStateAction<boolean>>;
-//   isRefreshFeeds: boolean;
-//   isOnline: boolean;
-// }
+export interface GlobalContextInterface {
+  setUser: Dispatch<
+    React.SetStateAction<Models.User<Models.Preferences> | null>
+  >;
+  setUserInfo: Dispatch<React.SetStateAction<UserType.Info>>;
+  setUserActivity: Dispatch<React.SetStateAction<UserType.Activity>>;
+  setFcmToken: Dispatch<React.SetStateAction<string>>;
+  setIsRefreshUserInfo: Dispatch<React.SetStateAction<boolean>>;
+  setIsRefreshFeeds: Dispatch<React.SetStateAction<boolean>>;
+  user: Models.User<Models.Preferences> | null;
+  userInfo: UserType.Info;
+  userActivity: UserType.Activity;
+  fcmToken: string;
+  isRefreshUserInfo: boolean;
+  isRefreshFeeds: boolean;
+  isLoading: boolean;
+  isOnline: boolean;
+}
 
-// const noUser: ExtendedUserType = {
-//   id: "",
-//   email: "",
-//   username: "",
-//   avatar_url: "",
-//   joined_at: "",
-//   posts: [],
-//   subscriptions: [],
-//   lines: [],
-//   viewed_notification_ids: [],
-//   role: "",
-// };
+const noUser: UserType.Info = {
+  id: "",
+  username: "",
+  avatar_url: "",
+  created_at: new Date(),
+  role: [],
+  name: [],
+};
 
-// const defaultValue: GlobalContextInterface = {
-//   user: null,
-//   setUser: (user: Models.User<Models.Preferences> | null) => {},
-//   isLoading: true,
-//   setUserInfo: (userInfo: ExtendedUserType) => {},
-//   userInfo: noUser,
-//   fcmToken: "",
-//   setFcmToken: (fcmToken: string) => {},
-//   setIsRefreshUserInfo: (isRefreshUserInfo: boolean) => {},
-//   isRefreshUserInfo: false,
-//   setIsRefreshFeeds: (isRefreshFeeds: boolean) => {},
-//   isRefreshFeeds: false,
-//   isOnline: true,
-// } as GlobalContextInterface;
+const noUserActivity: UserType.Activity = {
+  id: "",
+  viewed_notification_id: [],
+};
 
-// export const GlobalContext =
-//   createContext<GlobalContextInterface>(defaultValue);
+const defaultValue: GlobalContextInterface = {
+  setUser: (e: Models.User<Models.Preferences> | null) => {},
+  setUserInfo: (e: UserType.Info) => {},
+  setUserActivity: (e: UserType.Activity) => {},
+  setFcmToken: (fcmToken: string) => {},
+  setIsRefreshUserInfo: (isRefreshUserInfo: boolean) => {},
+  setIsRefreshFeeds: (isRefreshFeeds: boolean) => {},
+  user: null,
+  userInfo: noUser,
+  userActivity: noUserActivity,
+  fcmToken: "",
+  isRefreshUserInfo: false,
+  isRefreshFeeds: false,
+  isLoading: true,
+  isOnline: true,
+} as GlobalContextInterface;
 
-// export const useGlobalContext = () => useContext(GlobalContext);
+export const GlobalContext =
+  createContext<GlobalContextInterface>(defaultValue);
 
-// export const GlobalProvider = ({ children }: GlobalContextProps) => {
-//   const { isInternetReachable } = useNetworkState();
-//   const [user, setUser] = useState<Models.User<Models.Preferences> | null>(
-//     null
-//   );
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [userInfo, setUserInfo] = useState<ExtendedUserType>(noUser);
-//   const [fcmToken, setFcmToken] = useState<string>("");
-//   const [isRefreshUserInfo, setIsRefreshUserInfo] = useState(false);
-//   const [isRefreshFeeds, setIsRefreshFeeds] = useState(false);
-//   const [isOnline, setIsOnline] = useState(true);
+export const useGlobalContext = () => useContext(GlobalContext);
 
-//   useEffect(() => {
-//     const initialize = async () => {
-//       await requestNotificationPermissions();
-//       setupNotificationHandlers();
+export const GlobalProvider = ({ children }: GlobalContextProps) => {
+  const { isInternetReachable } = useNetworkState();
+  const [user, setUser] = useState<Models.User<Models.Preferences> | null>(
+    null
+  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [userInfo, setUserInfo] = useState<UserType.Info>(noUser);
+  const [userActivity, setUserActivity] =
+    useState<UserType.Activity>(noUserActivity);
+  const [fcmToken, setFcmToken] = useState<string>("");
+  const [isRefreshUserInfo, setIsRefreshUserInfo] = useState(false);
+  const [isRefreshFeeds, setIsRefreshFeeds] = useState(false);
+  const [isOnline, setIsOnline] = useState(true);
 
-//       getCurrentUser()
-//         .then(async (res) => {
-//           if (res) {
-//             setUser(res);
-//             const token = await getFCMToken(setFcmToken);
-//             if (token) {
-//               const userInfo = await getUserInfo(res.$id, true);
-//               setUserInfo(userInfo);
-//               await setupPushTarget(token, res, userInfo.subscriptions);
-//             }
-//           } else {
-//             setUser(null);
-//             setUserInfo(noUser);
-//           }
-//         })
-//         .catch((error) => console.log(error))
-//         .finally(() => setIsLoading(false));
-//     };
+  useEffect(() => {
+    const initialize = async () => {
+      await requestNotificationPermissions();
+      setupNotificationHandlers();
 
-//     initialize();
-//   }, []);
+      getCurrentUser()
+        .then(async (res) => {
+          if (res) {
+            setUser(res);
+            const token = await getFCMToken(setFcmToken);
+            if (token) {
+              setUserInfo(await getUserInfo(res.$id));
+              setUserActivity(await getUserActivity(res.$id));
+              await setupPushTarget(token, res);
+            }
+          } else {
+            setUser(null);
+            setUserInfo(noUser);
+          }
+        })
+        .catch((error) => console.log(error))
+        .finally(() => setIsLoading(false));
+    };
 
-//   useEffect(() => {
-//     const refresh = async () => {
-//       if (user) setUserInfo(await getUserInfo(user.$id, true));
-//     };
-//     if (isRefreshUserInfo) {
-//       refresh();
-//       setIsRefreshUserInfo(false);
-//     }
-//   }, [isRefreshUserInfo]);
+    initialize();
+  }, []);
 
-//   useEffect(() => {
-//     setIsOnline(!!isInternetReachable);
-//   }, [isInternetReachable]);
+  useEffect(() => {
+    const refresh = async () => {
+      if (user) setUserInfo(await getUserInfo(user.$id));
+    };
+    if (isRefreshUserInfo) {
+      refresh();
+      setIsRefreshUserInfo(false);
+    }
+  }, [isRefreshUserInfo]);
 
-//   return (
-//     <GlobalContext.Provider
-//       value={{
-//         isLoading,
-//         setUser,
-//         user,
-//         setUserInfo,
-//         userInfo,
-//         setFcmToken,
-//         fcmToken,
-//         setIsRefreshUserInfo,
-//         isRefreshUserInfo,
-//         setIsRefreshFeeds,
-//         isRefreshFeeds,
-//         isOnline,
-//       }}
-//     >
-//       {children}
-//     </GlobalContext.Provider>
-//   );
-// };
+  useEffect(() => {
+    setIsOnline(!!isInternetReachable);
+  }, [isInternetReachable]);
 
-// export default GlobalProvider;
+  return (
+    <GlobalContext.Provider
+      value={{
+        setUser,
+        setUserInfo,
+        setUserActivity,
+        setFcmToken,
+        setIsRefreshUserInfo,
+        setIsRefreshFeeds,
+        user,
+        userInfo,
+        userActivity,
+        fcmToken,
+        isLoading,
+        isRefreshUserInfo,
+        isRefreshFeeds,
+        isOnline,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default GlobalProvider;
