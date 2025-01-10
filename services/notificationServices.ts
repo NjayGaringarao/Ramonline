1e5c9cf2624c8f795c47631d8de5d500e30d62a6
njay <ninogaringarao13@gmail.com>
2025-01-10 13:37:32 +0800

    Modified (notificationServices) : Implement new implementation

--- services/notificationServices.ts
+++ services/notificationServices.ts
@@ -1,292 +1,314 @@
-// import { Models } from "react-native-appwrite";
-// import {
-//   _createTarget,
-//   _deleteTarget,
-//   _getCurrentUser,
-//   _getDocument,
-//   _getSession,
-//   _listSession,
-//   _subscribeTopic,
-//   _unsubscribeTopic,
-//   _updateDocument,
-// } from "./appwrite";
-// import {
-//   ExtendedUserType,
-//   LineType,
-//   NotificationType,
-// } from "@/constants/types";
-// import messaging from "@react-native-firebase/messaging";
-// import * as Notifications from "expo-notifications";
-// import { AppRegistry, Alert } from "react-native";
-// import { toNotificationList, toPushTargetList } from "@/lib/typeConverter";
-// import {
-//   COLLECTION_USER_ID,
-//   COLLECTION_USER_NOTIFICATION_ID,
-// } from "@/constants/variables";
+import { Models, Query } from "react-native-appwrite";
+import {
+  _createTarget,
+  _deleteDocument,
+  _deleteTarget,
+  _getCurrentUser,
+  _getDocument,
+  _getSession,
+  _listDocuments,
+  _listSession,
+  _subscribeTopic,
+  _unsubscribeTopic,
+  _updateDocument,
+} from "./appwrite";
+import messaging from "@react-native-firebase/messaging";
+import * as Notifications from "expo-notifications";
+import { Alert } from "react-native";
+import {
+  toLineInfoList,
+  toNotificationInfoList,
+  toPushTargetList,
+} from "@/lib/typeConverter";
+import { env } from "@/constants/env";
+import { LineType, NotificationType, UserType } from "@/types/models";
 
-// //#region Firebase Messaging
+//#region Notification Handling
 
-// // Request permission for notifications
-// export const requestNotificationPermissions = async () => {
-//   const { status } = await Notifications.requestPermissionsAsync();
-//   if (status !== "granted") {
-//     console.log("Notification permission not granted!");
-//   }
-// };
+// Request permission for notifications
+export const requestNotificationPermissions = async () => {
+  const { status } = await Notifications.requestPermissionsAsync();
+  if (status !== "granted") {
+    console.log("Notification permission not granted!");
+  }
+};
 
-// // Retrieve FCM token and request permission
-// export const getFCMToken = async (setFcmToken?: (token: string) => void) => {
-//   const authStatus = await messaging().requestPermission();
-//   const enabled =
-//     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-//     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+// Retrieve FCM token and request permission
+export const getFCMToken = async (setFcmToken?: (token: string) => void) => {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-//   if (enabled) {
-//     const fcmToken = await messaging().getToken();
-//     if (setFcmToken) setFcmToken(fcmToken);
-//     console.log(`FCM Token: ${fcmToken}`);
-//     return fcmToken;
-//   } else {
-//     console.log("Notification permission not granted for FCM.");
-//     return undefined;
-//   }
-// };
+  if (enabled) {
+    const fcmToken = await messaging().getToken();
+    if (setFcmToken) setFcmToken(fcmToken);
+    console.log(`FCM Token: ${fcmToken}`);
+    return fcmToken;
+  } else {
+    console.log("Notification permission not granted for FCM.");
+    return undefined;
+  }
+};
 
-// // Setup notification handlers
-// export const setupNotificationHandlers = () => {
-//   // Background message handler
-//   // messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-//   //   console.log("Message handled in the background:", remoteMessage);
-//   //   AppRegistry.registerComponent("index", () => {});
-//   // });
+// Setup notification handlers
+export const setupNotificationHandlers = () => {
+  // Background message handler
+  // messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  //   console.log("Message handled in the background:", remoteMessage);
+  //   AppRegistry.registerComponent("index", () => {});
+  // });
 
-//   // Handle notifications that open the app from a quit state
-//   messaging()
-//     .getInitialNotification()
-//     .then((remoteMessage) => {
-//       if (remoteMessage) {
-//         console.log(
-//           "Notification caused the app to open from quit state:",
-//           remoteMessage.notification
-//         );
-//       }
-//     });
+  // Handle notifications that open the app from a quit state
+  messaging()
+    .getInitialNotification()
+    .then((remoteMessage) => {
+      if (remoteMessage) {
+        console.log(
+          "Notification caused the app to open from quit state:",
+          remoteMessage.notification
+        );
+      }
+    });
 
-//   // Handle notifications that open the app from the background
-//   messaging().onNotificationOpenedApp((remoteMessage) => {
-//     console.log(
-//       "Notification caused app to open from background state:",
-//       remoteMessage.notification
-//     );
-//   });
+  // Handle notifications that open the app from the background
+  messaging().onNotificationOpenedApp((remoteMessage) => {
+    console.log(
+      "Notification caused app to open from background state:",
+      remoteMessage.notification
+    );
+  });
 
-//   // Handle notifications when the app is in the foreground
-//   messaging().onMessage(async (remoteMessage) => {
-//     Alert.alert("A new FCM message arrived", JSON.stringify(remoteMessage));
-//   });
+  // Handle notifications when the app is in the foreground
+  messaging().onMessage(async (remoteMessage) => {
+    Alert.alert("A new FCM message arrived", JSON.stringify(remoteMessage));
+  });
 
-//   Notifications.setNotificationHandler({
-//     handleNotification: async () => ({
-//       shouldShowAlert: true,
-//       shouldPlaySound: true,
-//       shouldSetBadge: true,
-//     }),
-//   });
-// };
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    }),
+  });
+};
 
-// //#endregion
+//#endregion
 
-// const getMatchedSessionTarget = async (
-//   targets: Models.Target[],
-//   session: Models.Session
-// ) => {
-//   let matchedTargets: Models.Target[] = [];
+//#region Notification Setup
+const getMatchedSessionTarget = async (
+  targets: Models.Target[],
+  session: Models.Session
+) => {
+  let matchedTargets: Models.Target[] = [];
 
-//   targets.forEach((target) => {
-//     if (target.$id == session.$id) {
-//       matchedTargets.push(target);
-//     }
-//   });
+  targets.forEach((target) => {
+    if (target.$id == session.$id) {
+      matchedTargets.push(target);
+    }
+  });
 
-//   if (matchedTargets.length == 1) {
-//     return matchedTargets[0];
-//   } else {
-//     return undefined;
-//   }
-// };
+  if (matchedTargets.length == 1) {
+    return matchedTargets[0];
+  } else {
+    return undefined;
+  }
+};
 
-// const createPushTarget = async (sessionId: string, fcmToken: string) => {
-//   try {
-//     return await _createTarget(sessionId, fcmToken);
-//   } catch (error) {
-//     throw Error(`createPushTarget : ${error}`);
-//   }
-// };
+const createPushTarget = async (sessionId: string, fcmToken: string) => {
+  try {
+    return await _createTarget(sessionId, fcmToken);
+  } catch (error) {
+    throw Error(`createPushTarget : ${error}`);
+  }
+};
 
-// const validateTargets = async (targets: Models.Target[]) => {
-//   try {
-//     const sessionList = await _listSession();
+const validateTargets = async (targets: Models.Target[]) => {
+  try {
+    const sessionList = await _listSession();
 
-//     // getting push targets
-//     const pushTargets: Models.Target[] = toPushTargetList(targets);
+    // getting push targets
+    const pushTargets: Models.Target[] = toPushTargetList(targets);
 
-//     // getting unused pushtargets
-//     let unusedPushTargets: Models.Target[] = [];
-//     pushTargets.forEach((target) => {
-//       const isUsed = sessionList.sessions.some((session) => {
-//         return session.$id === target.$id;
-//       });
+    // getting unused pushtargets
+    let unusedPushTargets: Models.Target[] = [];
+    pushTargets.forEach((target) => {
+      const isUsed = sessionList.sessions.some((session) => {
+        return session.$id === target.$id;
+      });
 
-//       if (!isUsed) {
-//         unusedPushTargets.push(target);
-//       }
-//     });
+      if (!isUsed) {
+        unusedPushTargets.push(target);
+      }
+    });
 
-//     // delete unused pushtargets (if there is any)
-//     if (unusedPushTargets.length) {
-//       unusedPushTargets.forEach(async (targets) => {
-//         await _deleteTarget(targets.$id);
-//       });
-//     }
-//   } catch (error) {
-//     throw Error(`validateTargetsWithSessions : ${error}`);
-//   }
-// };
+    // delete unused pushtargets (if there is any)
+    if (unusedPushTargets.length) {
+      unusedPushTargets.forEach(async (targets) => {
+        await _deleteTarget(targets.$id);
+      });
+    }
+  } catch (error) {
+    throw Error(`validateTargetsWithSessions : ${error}`);
+  }
+};
 
-// const resubscribeTopics = async (
-//   pushTargets: Models.Target[],
-//   userSubscriptions: LineType[]
-// ) => {
-//   // iterates all user subscriptions. each iteration, there is a iteration of pushtargets where the actual resubscription happens
-//   userSubscriptions.forEach((subscription) => {
-//     pushTargets.forEach(async (target) => {
-//       try {
-//         await _subscribeTopic(subscription.id, target.$id);
-//       } catch (error) {
-//         console.log(
-//           `setupNewPushTarget - topic ID : ${subscription.id} PushTarget ID: ${target.$id} \n Error : ${error}`
-//         );
-//       }
-//     });
-//   });
-// };
+const resubscribeTopics = async (
+  pushTargets: Models.Target[],
+  userSubscriptions: LineType.Info[]
+) => {
+  // iterates all user subscriptions. each iteration, there is a iteration of pushtargets where the actual resubscription happens
+  userSubscriptions.forEach((subscription) => {
+    pushTargets.forEach(async (target) => {
+      try {
+        await _subscribeTopic(subscription.id, target.$id);
+      } catch (error) {
+        console.log(
+          `setupNewPushTarget - topic ID : ${subscription.id} PushTarget ID: ${target.$id} \n Error : ${error}`
+        );
+      }
+    });
+  });
+};
 
-// // to be used when logging in
-// export const setupPushTarget = async (
-//   fcmToken: string,
-//   user: Models.User<Models.Preferences>,
-//   subscriptions: LineType[]
-// ) => {
-//   try {
-//     const session = await _getSession();
-//     const matchedSessionTarget = await getMatchedSessionTarget(
-//       user.targets,
-//       session
-//     );
+export const setupPushTarget = async (
+  fcmToken: string,
+  user: Models.User<Models.Preferences>
+) => {
+  try {
+    const lineSubscriptionDocuments = await _listDocuments(
+      env.DATABASE_PRIMARY,
+      env.COLLECTION_LINE_SUBSCRIPTION,
+      [Query.equal("user_id", user.$id)]
+    );
+    const lineSubscription = toLineInfoList(
+      lineSubscriptionDocuments.documents
+    );
 
-//     if (matchedSessionTarget) {
-//       if (fcmToken != matchedSessionTarget.identifier) {
-//         await _deleteTarget(matchedSessionTarget.$id);
-//         user.targets.push(await createPushTarget(session.$id, fcmToken));
-//       }
-//     } else {
-//       user.targets.push(await createPushTarget(session.$id, fcmToken));
-//     }
+    const session = await _getSession();
+    const matchedSessionTarget = await getMatchedSessionTarget(
+      user.targets,
+      session
+    );
 
-//     await validateTargets(user.targets);
-//     // do resubscription of all the targets into lists of subscriptions in user docs
-//     const newUser = await _getCurrentUser();
-//     const newPushTarget: Models.Target[] = toPushTargetList(newUser.targets);
-//     resubscribeTopics(newPushTarget, subscriptions);
-//     return newUser;
-//   } catch (error) {
-//     console.log(
-//       `notificationServices.ts => setupPushTarget :: ERROR : ${error}`
-//     );
-//   }
-// };
+    if (matchedSessionTarget) {
+      if (fcmToken != matchedSessionTarget.identifier) {
+        await _deleteTarget(matchedSessionTarget.$id);
+        user.targets.push(await createPushTarget(session.$id, fcmToken));
+      }
+    } else {
+      user.targets.push(await createPushTarget(session.$id, fcmToken));
+    }
 
-// //to be used when Logging out
-// export const deletePushTarget = async () => {
-//   try {
-//     const session = await _getSession();
-//     await _deleteTarget(session.$id);
-//   } catch (error) {
-//     console.log(
-//       `notificationServices.ts => deletePushTarget :: ERROR : ${error}`
-//     );
-//   }
-// };
+    await validateTargets(user.targets);
+    // do resubscription of all the targets into lists of subscriptions in user docs
+    const newUser = await _getCurrentUser();
+    const newPushTarget: Models.Target[] = toPushTargetList(newUser.targets);
+    resubscribeTopics(newPushTarget, lineSubscription);
+    return newUser;
+  } catch (error) {
+    console.log(
+      `notificationServices.ts => setupPushTarget :: ERROR : ${error}`
+    );
+  }
+};
 
-// export const getNotifications = async (
-//   user_id: string
-// ): Promise<NotificationType[]> => {
-//   try {
-//     let result: NotificationType[] = [];
-//     const notificationDocs = await _getDocument(
-//       COLLECTION_USER_NOTIFICATION_ID,
-//       user_id
-//     );
+//to be used when Logging out
+export const deletePushTarget = async () => {
+  try {
+    const session = await _getSession();
+    await _deleteTarget(session.$id);
+  } catch (error) {
+    console.log(
+      `notificationServices.ts => deletePushTarget :: ERROR : ${error}`
+    );
+  }
+};
 
-//     if (notificationDocs.notifications) {
-//       result = toNotificationList(notificationDocs.notifications);
-//     }
+//#endregion
 
-//     return result;
-//   } catch (error) {
-//     console.error(
-//       `notificationServices.ts => getNotification :: ERROR : ${error}`
-//     );
-//     return [];
-//   }
-// };
+//#region Notification Database
 
-// export const setNotificationViewed = async (
-//   userInfo: ExtendedUserType,
-//   notification_id: string
-// ) => {
-//   try {
-//     await _updateDocument(COLLECTION_USER_ID, userInfo.id, {
-//       viewed_notification_ids: [
-//         ...userInfo.viewed_notification_ids,
-//         notification_id,
-//       ],
-//     });
-//   } catch (error) {
-//     console.error(
-//       `notificationServices.ts => setNotificationViewed :: ERROR : ${error}`
-//     );
-//   }
-// };
+export const getNotifications = async (user_id: string, last_id?: string) => {
+  try {
+    if (last_id) {
+      const notification = await _listDocuments(
+        env.DATABASE_PRIMARY,
+        env.COLLECTION_NOTIFICATION_INFO,
+        [
+          Query.orderDesc("created_at"),
+          Query.limit(5),
+          Query.cursorAfter(last_id),
+        ]
+      );
 
-// export const deleteNotification = async (
-//   userInfo: ExtendedUserType,
-//   notifications: NotificationType[]
-// ) => {
-//   const notification_ids: string[] = notifications.map(
-//     (notification) => notification.id
-//   );
+      return toNotificationInfoList(notification.documents);
+    } else {
+      const notification = await _listDocuments(
+        env.DATABASE_PRIMARY,
+        env.COLLECTION_NOTIFICATION_INFO,
+        [Query.equal("user_id", user_id), Query.orderDesc("created_at")]
+      );
 
-//   try {
-//     await _updateDocument(COLLECTION_USER_ID, userInfo.id, {
-//       viewed_notification_ids: userInfo.viewed_notification_ids.filter(
-//         (id) => !notification_ids.includes(id)
-//       ),
-//     });
+      return toNotificationInfoList(notification.documents);
+    }
+  } catch (error) {
+    console.error(
+      `notificationServices.ts => getNotification :: ERROR : ${error}`
+    );
+    return [];
+  }
+};
 
-//     const user_notifications = await getNotifications(userInfo.id);
-//     const userNotification_ids = user_notifications.map(
-//       (notification) => notification.id
-//     );
+export const setNotificationViewed = async (
+  userActivity: UserType.Activity,
+  notification_id: string
+) => {
+  try {
+    await _updateDocument(
+      env.DATABASE_PRIMARY,
+      env.COLLECTION_USER_ACTIVITY,
+      userActivity.id,
+      {
+        viewed_notification_ids: [
+          ...userActivity.viewed_notification_id,
+          notification_id,
+        ],
+      }
+    );
+  } catch (error) {
+    console.error(
+      `notificationServices.ts => setNotificationViewed :: ERROR : ${error}`
+    );
+  }
+};
 
-//     await _updateDocument(COLLECTION_USER_NOTIFICATION_ID, userInfo.id, {
-//       notifications: userNotification_ids.filter(
-//         (notif) => !notification_ids.includes(notif)
-//       ),
-//     });
-//   } catch (error) {
-//     console.error(
-//       `notificationServices.ts => deleteNotification :: ERROR : ${error}`
-//     );
-//   }
-// };
+export const deleteNotification = async (
+  userActivity: UserType.Activity,
+  notification_id: string
+) => {
+  try {
+    await _deleteDocument(
+      env.DATABASE_PRIMARY,
+      env.COLLECTION_NOTIFICATION_INFO,
+      notification_id
+    );
+
+    await _updateDocument(
+      env.DATABASE_PRIMARY,
+      env.COLLECTION_USER_ACTIVITY,
+      userActivity.id,
+      {
+        viewed_notification_id: userActivity.viewed_notification_id.filter(
+          (id) => !notification_id.includes(id)
+        ),
+      }
+    );
+  } catch (error) {
+    console.error(
+      `notificationServices.ts => deleteNotification :: ERROR : ${error}`
+    );
+  }
+};
+
+//#endregion
