b5a470e2ff6ffb369be7c4affaaa1624917ea288
njay <ninogaringarao13@gmail.com>
2025-01-11 23:32:17 +0800

    Modified (index) : implement new styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,50 @@
-import { env } from "@/constants/env";
-import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, Image } from "react-native";
+import { useGlobalContext } from "@/context/GlobalProvider";
+import CustomButton from "@/components/CustomButton";
+import { colors, images } from "@/constants";
+import React, { useEffect } from "react";
+
 export default function Index() {
-  const [variable, setVariable] = useState("");
+  const { isLoading, user } = useGlobalContext();
 
   useEffect(() => {
-    setVariable(env.APPWRITE_ENDPOINT!);
-  }, []);
+    if (!isLoading && user) {
+      if (!user.emailVerification) {
+        router.replace("/(auth)/verification");
+      } else {
+        router.replace("/home");
+      }
+    }
+  }, [isLoading, user]);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-xl">APPWRITE ENDPOINT :{` ${variable}`}</Text>
-    </View>
+    <>
+      <View className="flex flex-1 bg-background items-center justify-center relative">
+        <Image
+          source={images.gate}
+          className="absolute h-full w-full opacity-20"
+        />
+        <View className="flex-1 items-center justify-center">
+          <Image source={images.logo} className="h-64 w-64" />
+          <Text className=" font-extrabold text-primary text-6xl mt-2">
+            Ramonline
+          </Text>
+          <Text className="font-bold text-lg text-gray-700 -mt-2">
+            Making ramonians more connected
+          </Text>
+        </View>
+        <View className="w-full items-center p-6">
+          <CustomButton
+            title="Continue"
+            handlePress={() => router.replace("/signIn")}
+            containerStyles="w-full h-12"
+            isLoading={isLoading}
+          />
+        </View>
+      </View>
+      <StatusBar backgroundColor={colors.background} style="auto" />
+    </>
   );
 }
